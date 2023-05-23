86621cf49127c0cc5ad32ae4712c3e490ecbe50b
Cecile Ferrier <cgmferrier@gmail.com>
2023-05-23 14:11:29 +0100

    WIP - fixing routing

--- src/components/recipe-list/recipe-list.component.tsx
+++ src/components/recipe-list/recipe-list.component.tsx
@@ -2,7 +2,6 @@
 
 import LoadingSpinner from '@/components/common/loading-spinner/loading-spinner.component';
 import { RecipeService } from '@/shared/services/recipe.service';
-import Link from 'next/link';
 import { FormEvent, useEffect, useState } from 'react';
 import './recipe-list.component.scss';
 
@@ -27,6 +26,10 @@ const RecipeList = () => {
     });
   }
 
+  const goToDetails = () => {
+    console.log('hi')
+  }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
@@ -45,11 +48,11 @@ const RecipeList = () => {
       </form>
       <ul className="recipe-list">{recipes && recipes.map(({ id, image, title }) => {
         return (
-          <li className="recipe" key={id}>
-            <Link href={`/recipe/${id}`}>
+          <li className="recipe" key={id} onClick={goToDetails}>
+            {/*<Link href={`/recipe/${id}`}>*/}
               <div className="recipe-title">{title}</div>
               <img className="recipe-img" alt={title} src={image}/>
-            </Link>
+            {/*</Link>*/}
           </li>
         )
       })}
